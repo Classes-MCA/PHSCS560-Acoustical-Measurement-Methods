768ef729d72f00761e0df9ec81dcf45fabda8070
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-11-12 15:13:43 -0700

    updated ArrayAnalysis

--- Lab_9_Analysis.m
+++ Lab_9_Analysis.m
@@ -1,3 +1,12 @@
 clear; close all;
 
+% Gathering the data into an array
 tubeArray = MicArray;
+tubeArray.initializeArray(4)
+CHnums = [0, 1, 2, 3];
+
+%----- Sound Speed Measurement -----%
+micLocations = [-11, -6, 6, 11];
+
+% Gather in the data
+for i = 1:length(
