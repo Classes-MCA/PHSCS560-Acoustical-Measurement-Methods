ed930c057aaf9a0efbcaad4bf6318191325dd965
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-11-23 10:07:24 -0700

    Corrected logarithm errors

--- Lab 8/Run_Analyses.m
+++ Lab 8/Run_Analyses.m
@@ -3,11 +3,20 @@ clear; close all; plotStyle()
 Sound_Power_Reverberation_Room;
 Sound_Power_Anechoic_Chamber;
 
-figure(); hold on;
+%% Final calculations
+figure()
 semilogx(frequencies,SoundPower_Reverb,'b-o','LineWidth',2,'DisplayName','Reverberation Chamber')
+hold on
 semilogx(frequencies,SoundPower_Anechoic,'r-o','LineWidth',2,'DisplayName','Anechoic Chamber')
 title('Blender Sound Power')
-xlabel('OTO Frequency (Hz)')
+xlabel('OTO Band Center Frequency (Hz)')
 ylabel('Sound Power (dB)')
 grid on
-legend()
+legend('Location','SouthEast')
+
+% Calculating the total sound powers
+pressure_reverb = Iref.*10.^(SoundPower_Reverb ./ 10);
+pressure_anechoic = Iref.*10.^(SoundPower_Anechoic ./ 10);
+
+TotalSoundPower_Reverb = 10 * log10(sum(pressure_reverb)/Iref)
+TotalSoundPower_Anechoic = 10 * log10(sum(pressure_anechoic)/Iref)
