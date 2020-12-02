9ce89089cd1063ebc792999cace8d1ebad3fbab6
Mark C. Anderson <anderson.mark.az@gmail.com>
2020-12-02 10:35:20 -0700

    Analysis Complete

---
+++ Lab 11/R2D2_Analysis.m
@@ -0,0 +1,108 @@
+clear; close all;
+
+usePackage GeneralSignalProcessing; plotStyle()
+usePackage ArrayAnalysis
+usePackage SourceModels
+
+pathToData = '/Volumes/Mark Drive/Classes/Acoustical-Measurement-Methods/Lab 11/560 Lab 11 Data/R2D2Meas';
+
+mics = MicArray;
+
+% mics.Microphones(1) = Microphone; % FIXME: Why is it initialized with a dinosaur sound???
+
+mics.initializeArray(61);
+
+% Array Details
+fs = 44.1e3;
+range = 3.0; % How long the array is in meters
+numMics = 61; % Number of microphones
+targetFrequency = 1000; % Hz
+angles = -90:10:90;
+for i = 1:61
+    
+    disp(strcat("Reading Channel ",num2str(i)))
+    
+    filename = strcat(pathToData,filesep,"R2D2_Meas_M",num2str(i),".wav");
+    
+    mics.Microphones(i).Waveform = audioread(filename);
+                                           
+    mics.Microphones(i).SamplingFrequency = fs;
+    
+    mics.Microphones(i).Location = [-range/2 + (i-1)*range/(numMics-1), 0, 0];
+    
+    mics.Microphones(i).Name = strcat("Channel ",num2str(i));
+    
+end
+
+% Character Locations
+characterLocations = [-80, 0;
+                      -80,40;
+                      -80,60;
+                      -60,60;
+                      -40,95;
+                        0,80;
+                       30,80;
+                       60,95;
+                       80,60;
+                       75,10];
+% scatter(characterLocations(:,1),characterLocations(:,2))
+characterAngles = atan(characterLocations(:,1)./characterLocations(:,2)) * 180/pi;
+
+% Getting the pattern
+beamform(mics,angles,targetFrequency)
+%%
+% making the plot pretty
+hold on
+h = gcf;
+h.Units = "Inches";
+h.Position = [2,2,12,5];
+ax = gca;
+ax.Units = "Inches";
+ax.Position = [1,0.5,7,4]
+ax.Title.String = strcat("Cantina Directivity at ", num2str(targetFrequency), " Hz");
+ax.Title.FontSize = 20;
+
+for i = 1:length(characterAngles)
+    
+    theta = [characterAngles(i),characterAngles(i)];
+    r = [-100,0];
+    
+    polarplot(theta,r,'LineStyle','--','Color',[0.5,0.5,0.5])
+    
+end
+
+hleg = legend({'Measured Directivity',['Map-based' newline 'Location Estimates']});
+hleg.FontSize = 15;
+hleg.Title.String = 'Legend';
+hleg.Units = "Inches";
+hleg.Position = [hleg.Position(1),hleg.Position(2),3,1.5];
+
+%%
+% Over-riding the angle calculations
+characterAngles = [60, 44, -2, -21, -34, -49, -55, -83]
+
+audioArray = MicArray;
+audioArray.initializeArray(length(characterAngles));
+%audioArray.Microphones(1) = Microphone; % FIXME: Why is it initialized with a dinosaur sound???
+for i = 1:length(characterAngles)
+    
+    audioArray.Microphones(i).Waveform = getAudioAtAngle(mics,characterAngles(i));
+    audioArray.Microphones(i).SamplingFrequency = fs;
+    
+end
+
+% Results from using the trigonometry angles
+% 1 - C3PO
+% 2 - 
+% 3 - Something about paying money when reaching orbit
+% 4 - 
+% 5 - "I don't like you!"
+% 6 - Looking for Yoda
+% 7 - "We don't serve their kind here"
+% 8 - 
+% 9 - "Han Solo"
+% 10 - Just a bunch of music
+% 
+% Specific Angle Results
+% -34: "Where is the Rebel Base?" <--- FOUND HIM!!!
+soundsc(getAudioAtAngle(mics,-34),fs)
---
+++ Lab 11/Testing_Beamforming.m
@@ -0,0 +1,71 @@
+clear; close all; plotStyle()
+
+pathToData = '/Volumes/Mark Drive/Classes/Acoustical-Measurement-Methods/Lab 11/560 Lab 11 Data';
+
+mics = MicArray;
+
+% Loading in the data
+channels = 0:4;
+fs = 51200;
+range = 0.322; % How long the array is in meters
+numMics = 5; % Number of microphone
+angles = -90:1:90;
+targetFrequency = 1000; % Hz
+steerAngles = [-60,2,80];
+IDnums = [2,1,3];
+targetAngles = [-45,0,90];
+
+
+h = figure();
+for i = 1:length(IDnums)
+    
+    IDnum = IDnums(i);
+    
+    for j = 1:5
+    
+    mics.Microphones(j).Waveform = binfileload(pathToData,...
+                                               'ID',...
+                                               IDnum,...
+                                               channels(j));
+                                           
+    mics.Microphones(j).SamplingFrequency = fs;
+    
+    mics.Microphones(j).Location = [-range/2 + (j-1)*range/(numMics-1), 0, 0];
+    
+    end
+    
+    subplot(2,2,i)
+    beamform(mics,angles,targetFrequency)
+    hold on
+    
+    H = linearArray(numMics,range/4,targetFrequency,angles,steerAngles(i));
+    polarplot(angles*pi/180,20*log10(abs(H)),...
+              'DisplayName',strcat("Calculated Directivity at ",num2str(steerAngles(i)),"\circ"),...
+              'LineStyle','--',...
+              'Color','k')
+          
+    ax = gca;
+    ax.RLim = [-25,0];
+    ax.Title.String = strcat(num2str(targetAngles(i)),"\circ");
+    ax.Title.FontSize = 15;
+          
+          
+    
+end
+
+%%
+
+hleg = legend({"Measured",['Calculated for' newline 'Apparent Actual Angle']});
+hleg.Title.String = 'Legend';
+hleg.Units = "Inches";
+hleg.Position = [5.8,0.7,3,1.5];
+hleg.FontSize = 15;
+
+h.Units = "Inches";
+h.Position = [2,2,10,6];
+hleg.FontSize = 15;
+
+hstitle = sgtitle('Test Array Results');
+hstitle.FontSize = 18;
+hstitle.FontWeight = 'bold';
+hstitle.FontName = 'Arial';
